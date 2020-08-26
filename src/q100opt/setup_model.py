8d6864c0399d6fe8b5aad1a6989a8ab0a87b39b8
Johannes Roeder <johannes.roeder@uni-bremen.de>
2020-08-26 15:21:51 +0200

    Add first function for reading csv

--- src/q100opt/setup_model.py
+++ src/q100opt/setup_model.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 
-"""Function for reading predefined data and setting up an oemof-solph EnergySystem.
+"""
+Function for reading predefined data and setting up an oemof-solph EnergySystem.
 
 SPDX-License-Identifier: MIT
 
 """
+import os
+import pandas as pd
+
+
+def load_csv_data(path):
+
+    dict = {}
+
+    for name in os.listdir(path):
+
+        key = name.split('.csv')[0]
+        val = pd.read_csv(os.path.join(path, name))
+        dict.update([(key, val)])
+
+    return dict
