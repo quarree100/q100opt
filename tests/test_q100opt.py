a26a0bc8253249e1d4a14622891bdb3965d24fa5
Johannes Roeder <johannes.roeder@uni-bremen.de>
2020-08-26 16:20:32 +0200

    Add import csv test

--- tests/test_q100opt.py
+++ tests/test_q100opt.py
@@ -1,6 +1,19 @@
 
+import os
 from q100opt.cli import main
+from nose.tools import eq_
+from q100opt.setup_model import load_csv_data
+
+
+basedir = os.path.dirname(__file__)
 
 
 def test_main():
     assert main([]) == 0
+
+
+def test_import_csv():
+    dir_import = os.path.join(basedir, '_files/import_csv')
+    dic = load_csv_data(dir_import)
+    eq_(dic['Source']['label'][0], 'heat')
+
