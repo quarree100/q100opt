91187dc21d169623ee32144264f57dadb7ca352e
Johannes Roeder <johannes.roeder@uni-bremen.de>
2020-08-27 13:10:35 +0200

    Add tests for check active components function

--- tests/test_q100opt.py
+++ tests/test_q100opt.py
@@ -1,8 +1,11 @@
 
 import os
 
+import pandas as pd
+
 from q100opt.cli import main
 from q100opt.setup_model import load_csv_data
+from q100opt.setup_model import check_active
 
 basedir = os.path.dirname(__file__)
 
@@ -15,3 +18,16 @@ def test_import_csv():
     dir_import = os.path.join(basedir, '_files/import_csv')
     dic = load_csv_data(dir_import)
     assert dic['Source']['label'][0] == 'heat'
+
+
+def test_check_active():
+    d = {'Something': pd.DataFrame([[4, 1], [3, 0]], columns=['A', 'active'])}
+    d = check_active(d)
+    assert ((len(d['Something'].index) == 1) and
+            ('active' not in d['Something'].columns))
+
+
+def test_no_active():
+    d = {'Something': pd.DataFrame([[4, 1], [3, 0]], columns=['A', 'B'])}
+    d_new = check_active(d)
+    assert (d == d_new)
