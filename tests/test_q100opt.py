b0a52230106310b04c73429720b943c6c45f21ff
Johannes Roeder <johannes.roeder@uni-bremen.de>
2020-08-27 16:11:12 +0200

    Add test for add Bus function

--- tests/test_q100opt.py
+++ tests/test_q100opt.py
@@ -1,9 +1,11 @@
 
 import os
 
+import oemof.solph as solph
 import pandas as pd
 
 from q100opt.cli import main
+from q100opt.setup_model import add_buses
 from q100opt.setup_model import check_active
 from q100opt.setup_model import load_csv_data
 
@@ -31,3 +33,12 @@ def test_no_active():
     d = {'Something': pd.DataFrame([[4, 1], [3, 0]], columns=['A', 'B'])}
     d_new = check_active(d)
     assert (d == d_new)
+
+
+def test_add_bus():
+    data_bus = pd.DataFrame(
+        [['label_1', 0, 0, 0, 0], ['label_2', 0, 0, 0, 0]],
+        columns=['label', 'excess', 'shortage', 'shortage_costs',
+                 'excess_costs'])
+    n, b = add_buses(data_bus)
+    assert ((len(n) == 2) and (isinstance(b['label_1'], solph.Bus)))
