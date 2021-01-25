49d68e78217de82cb9c3c7ed79bea4d664438fd6
Johannes Roeder <johannes.roeder@uni-bremen.de>
2021-01-25 19:00:56 +0100

    Add some plots

--- src/q100opt/postprocessing.py
+++ src/q100opt/postprocessing.py
@@ -32,3 +32,35 @@ def plot_invest_storages(results):
     plt.ylabel('Installed Storage Capacity [kWh]')
     plt.xlabel('Label of storage')
     plt.show()
+
+
+def plot_buses(res=None, es=None):
+
+    l_buses = []
+
+    for n in es.nodes:
+        type_name =\
+            str(type(n)).replace("<class 'oemof.solph.", "").replace("'>", "")
+        if type_name == "network.Bus":
+            l_buses.append(n.label)
+
+    for n in l_buses:
+        bus_sequences = solph.views.node(res, n)["sequences"]
+        bus_sequences.plot(kind='line', drawstyle="steps-mid", subplots=False,
+                           sharey=True)
+        plt.show()
+
+
+def plot_storages_soc(res=None):
+
+    nodes = [x for x in res.keys() if x[1] is None]
+    node_storage_invest_label = [x[0].label for x in nodes if isinstance(
+        x[0], solph.components.GenericStorage)
+                           if hasattr(res[x]['scalars'], 'invest')]
+
+    for n in node_storage_invest_label:
+        soc_sequences = solph.views.node(res, n)["sequences"]
+        soc_sequences = soc_sequences.drop(soc_sequences.columns[[0, 2]], 1)
+        soc_sequences.plot(kind='line', drawstyle="steps-mid", subplots=False,
+                           sharey=True)
+        plt.show()
