9fe509b43ca4ad8bef8c2e08addc3544aa4a0f64
Johannes Roeder <johannes.roeder@uni-bremen.de>
2021-05-03 17:21:48 +0200

    Add new buildings module

---
+++ src/q100opt/buildings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""This module holds classes and function for optimising the energy
+ system of single buildings.
+
+Please use this module with care. It is work in progress and not tested yet!
+
+Contact: Johannes Röder <johannes.roeder@uni-bremen.de>
+
+SPDX-License-Identifier: MIT
+
+"""
