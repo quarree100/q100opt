42e79bf9f7ed8c1832527e10abe49f228b4882e9
Johannes Roeder <johannes.roeder@uni-bremen.de>
2021-05-03 18:26:49 +0200

    Change module header

--- src/q100opt/buildings.py
+++ src/q100opt/buildings.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-"""Module for optimising the energy system of single buildings.
+"""
+Module for optimising the energy system of single buildings.
 
 Please use this module with care. It is work in progress and not tested yet!
 
