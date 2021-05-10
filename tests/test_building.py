4c93796c6b1cae9528484b99bfd520cea9d40d2d
Johannes Roeder <johannes.roeder@uni-bremen.de>
2021-05-10 14:45:16 +0200

    Add test error precalc pv

--- tests/test_building.py
+++ tests/test_building.py
@@ -1,5 +1,14 @@
+import pytest
+
 from q100opt.buildings import Building
 
 
 def test_building_init():
     Building()
+
+
+def test_no_roof_data_error():
+    house = Building()
+    with pytest.raises(ValueError,
+                       match="Please provide roof data for a pre-calulation"):
+        house.precalc_pv_profiles()
