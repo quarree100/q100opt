8206dfb0638545af22882c0c35da8daa2adb02f7
Johannes Roeder <johannes.roeder@uni-bremen.de>
2021-05-26 11:24:39 +0200

    Add test for add battery function

--- tests/test_building.py
+++ tests/test_building.py
@@ -1,6 +1,8 @@
 import pytest
+import pandas as pd
 
-from q100opt.buildings import Building
+from q100opt.buildings import Building, DEFAULT_TABLE_COLLECTION_1,\
+    _add_battery_storage
 
 
 def test_building_init():
@@ -12,3 +14,27 @@ def test_no_roof_data_error():
     with pytest.raises(ValueError,
                        match="Please provide roof data for a pre-calulation"):
         house.calc_pv_profiles()
+
+
+def test_add_battery():
+    storages = DEFAULT_TABLE_COLLECTION_1['Storages']
+    storages.set_index("label", inplace=True)
+    tech_data = pd.Series(
+        [1, 3, 4, 0.5, 0.81, 0.0001],
+        index=[
+            "ep_costs", "offset", "minimum",
+            "c-rate", "eta-storage", "loss-rate-1/h"
+        ],
+    )
+    storages = _add_battery_storage(storages, tech_data, 10, 10)
+    bat_sto = storages.loc["battery-storage"].dropna()
+    assert bat_sto.equals(pd.Series(
+        [1, 0, "b_elec", 10, 0.0001, 0.9, 0.9, 0.5, 0.5, 1, 10, 4, 3],
+        index=["active", "investment", "bus",
+               "storage.nominal_storage_capacity", "storage.loss_rate",
+               "storage.inflow_conversion_factor",
+               "storage.outflow_conversion_factor",
+               "storage.invest_relation_input_capacity",
+               "storage.invest_relation_output_capacity", "invest.ep_costs",
+               "invest.maximum", "invest.minimum", "invest.offset"]
+    ))
